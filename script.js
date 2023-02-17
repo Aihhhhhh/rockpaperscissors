6129efa8b413ca3edae2c7e0e807930ea3626347
Aihhhhhh <mwanikidenno2@gmail.com>
2023-02-17 15:53:58 +0300

    Edit the playGame function to query select using the element's classes

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const game = ()=>{
     let moves = 0;
 
     const playGame = ()=>{
-        const rockBtn = document.querySelector('#rock');
-        const paperBtn = document.querySelector('#paper');
-        const scissorsBtn = document.querySelector('#scissors');
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorsBtn = document.querySelector('.scissors');
         const playerOptions = [rockBtn, paperBtn, scissorsBtn];
         const computerOptions = ['rock', 'paper', 'scissors'];
 
