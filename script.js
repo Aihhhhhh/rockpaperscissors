fe38fbd5cd471c025f9f2ffa7ae42bba0102c1a4
Aihhhhhh <mwanikidenno2@gmail.com>
2023-02-28 17:55:47 +0300

    edit the function that determines winner

--- script.js
+++ script.js
@@ -83,12 +83,12 @@ const pSign = document.getElementById('playerSign');
         chooseMove.innerText ='Game over!!';
         movesLeft.style.display = 'none';
 
-        if (playerScore > computerScore){
+        if (playerScore > computerScore && playerScore === 5){
             result.innerText= 'You won the game!!';
             result.style.fontSize = '2rem';
             result.style.color = 'green';
         }
-        else if (playerScore < computerScore){
+        else if (playerScore < computerScore && computerScore ===5){
             result.innerText ='You lost the game!!';
             result.style.fontSize ='2rem';
             result.style.color = 'red';
@@ -106,106 +106,11 @@ const pSign = document.getElementById('playerSign');
         })
     }
 
-    //calling the playRound function
+    //calling the playRound function inside the game function
     playGame();
 };
 
 game();
 
 
-    
-
-
-
-    
-
-
-
-
-
-
-/*function getComputerChoice(){
-    let result = Math.random();
-    if (result <= 0.34 && result >=0){
-        alert('Rock');
-    }else if (result <= 0.67 && result >=0.34){
-        alert('Scissors');
-    }else{
-        result <=1 && result >=0.67
-        alert('Paper');
-    }
-}
-
-function playRound (playAns, compAns){
-    if (playAns.toLowerCase() === 'rock'){
-        if (compAns === 'Paper'){
-            computerScore++;
-            return lose;
-        }else if (compAns ==='Rock'){
-            return tie;
-        }else{
-            playerScore++;
-            return win;
-        }
-    }if (playAns.toLowerCase() === 'paper'){
-        if (compAns === 'Paper'){
-            return tie;
-        }else if (compAns === 'Rock'){
-            playerScore++;
-            return win;
-        }else{
-            computerScore++;
-            return lose;
-        }
-    } if (playAns.toLowerCase() === 'scissors'){
-        if (compAns ==='Scissors'){
-            return tie;
-        }else  if (compAns === 'Paper'){
-            playerScore++;
-            return win;
-        }else{
-            computerScore++;
-            return lose;
-        }
-    }
-}
-
-let playerScore = parseInt(0);
-let computerScore = parseInt(0);
-let win = 'You win';
-let lose = 'You lose,computer wins';
-let tie = 'It is a tie';
-
-const button1 = document.createElement('playerb');
-const button2 = document.createElement('compb');
-const button3 = document.createElement('scoresb');
-document.body.appendChild(button1);
-document.body.appendChild(button2);
-document.body.appendChild(button3);
-
-
-
-button3.addEventListener('click', ()=>{
-    console.log(playRound(playAns, compAns));
-    console.log("your score =" +playerScore);
-    console.log("computer score =" +computerScore);
-})
-
-button2.addEventListener('click', ()=>{
-    console.log(playRound(playAns , compAns));
-    console.log("your score =" +playerScore);
-    console.log("computer score =" +computerScore);
-});
-
-/*function game(){
-    for (let i = 0; i< 5; i++){
-        let playAns = prompt(`What's your choice?`);
-        const compAns = getComputerChoice();
-        console.log(playRound(playAns, compAns));
-        console.log("your score =" +playerScore)
-        console.log("computer score = " +computerScore);
-    }
-}
-console.log(game());
-*/
 
